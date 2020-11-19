7c82b448a6a12325b6824457fcbd4cb58c4535ba
Dominique Heinelt <d.heinelt@nodus-it.de>
2020-11-19 12:50:01 +0100

    Fixed validations for selects

--- src/resources/views/livewire/bootstrap/components/types/select.blade.php
+++ src/resources/views/livewire/bootstrap/components/types/select.blade.php
@@ -1,18 +1,20 @@
-<div class="nodus-form-control" id="{{ $input->getId() }}_container" wire:ignore>
-    <select name="{{ $input->getName() }}"
-            class="form-control selectpicker @if($errors->has($input->getViewId())) is-invalid @endif"
-            @if($input->getMultiple()) multiple @endif
-            data-size="10"
-            data-live-search="true"
-            data-deselect-all-text="{{ $input->getDeselectAllText() }}"
-            data-select-all-text="{{ $input->getSelectAllText() }}"
-            data-none-selected-text="{{ $input->getNoneSelectedText() }}"
-            data-none-results-text="{{ $input->getNoneResultsText() }}"
-            wire:model="{{ $input->getViewId() }}">
-        @foreach($input->getValues() as $key => $option)
-            <option value="{{ $key }}" data-icon="{{ $option['icon'] }}">{{ $option['label'] }}</option>
-        @endforeach
-    </select>
+<div class="nodus-form-control" id="{{ $input->getId() }}_container" >
+    <div wire:ignore>
+        <select name="{{ $input->getName() }}"
+                class="form-control selectpicker @if($errors->has($input->getViewId())) is-invalid @endif"
+                @if($input->getMultiple()) multiple @endif
+                data-size="10"
+                data-live-search="true"
+                data-deselect-all-text="{{ $input->getDeselectAllText() }}"
+                data-select-all-text="{{ $input->getSelectAllText() }}"
+                data-none-selected-text="{{ $input->getNoneSelectedText() }}"
+                data-none-results-text="{{ $input->getNoneResultsText() }}"
+                wire:model="{{ $input->getViewId() }}">
+            @foreach($input->getValues() as $key => $option)
+                <option value="{{ $key }}" data-icon="{{ $option['icon'] }}">{{ $option['label'] }}</option>
+            @endforeach
+        </select>
+    </div>
     @include('nodus.packages.livewire-forms::livewire.'.config('livewire-forms.theme').'.components.validation')
 </div>
 
