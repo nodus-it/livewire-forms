9435f10e0f21533b199b17a4b9b94da0c0e70281
Dominique Heinelt <dominique.heinelt@nano-consult.de>
2021-02-04 18:03:54 +0100

    Fixed the code input

--- src/resources/views/livewire/bootstrap/components/types/code.blade.php
+++ src/resources/views/livewire/bootstrap/components/types/code.blade.php
@@ -1,13 +1,11 @@
 <div class="nodus-form-control" id="{{ $input->getId(true) }}_container">
     <div wire:ignore>
-        <textarea id="{{ $input->getId(true) }}">
-            {!! $this->values[$input->getId()] !!}
-        </textarea>
+        <textarea id="{{ $input->getId(true) }}">{!! $this->values[$input->getId()] !!}</textarea>
     </div>
     <textarea name="{{ $input->getId() }}"
               id="{{ $input->getId(true) }}_text"
               class="d-none"
-              wire:model.lazy="{{ $input->getViewId() }}"
+              wire:model.defer="{{ $input->getViewId() }}"
     >{!! $this->values[$input->getId()] !!}</textarea>
     @include('nodus.packages.livewire-forms::livewire.'.config('livewire-forms.theme').'.components.hint')
     @include('nodus.packages.livewire-forms::livewire.'.config('livewire-forms.theme').'.components.validation')
@@ -18,15 +16,14 @@
         function init() {
             const element = document.querySelector('#{{ $input->getId(true) }}');
             const input = document.querySelector('#{{ $input->getId(true) }}_text');
-
-            const view = CodeMirror.fromTextArea(element, {
+            const editor = CodeMirror.fromTextArea(element, {
                 mode: (@json($input->getMode())),
                 lineNumbers: true,
             });
 
-            view.on("change", function(){
+            editor.on("change", function(){
                 input.dispatchEvent(new CustomEvent('input',{
-                    detail: view.getValue(),
+                    detail: editor.getValue(),
                     bubbles: true,
                 }));
             })
